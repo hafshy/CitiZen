1eaccc319631a9dc878a479e058739086262fe0f
hafshy <hafshy@gmail.com>
2022-06-21 11:30:22 +0700

    Initial Commit

---
+++ CitiZen/CitiZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiZenApp.swift
+//  CitiZen
+//
+//  Created by Hafshy Yazid Albisthami on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CitiZenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
