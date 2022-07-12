774356d97b5e3fba54f53945e63eab832c694df3
Hafshy <hafshy@gmail.com>
2022-07-12 08:23:33 +0700

    feat: add chat model

---
+++ CitiZen/Model/ChatModel/ChatModel.swift
@@ -0,0 +1,36 @@
+//
+//  ChatViewModel.swift
+//  CitiZen
+//
+//  Created by Hafshy Yazid Albisthami on 11/07/22.
+//
+
+import Foundation
+
+struct Chat: Identifiable {
+    var id: Int
+    var landmarkName: String
+    var landmarkIconName: String
+    var message: [Message]
+}
+
+struct Message: Identifiable {
+    enum MessageType {
+        case Sent, Received
+    }
+    
+    var id = UUID()
+    let date: Date
+    let text: String
+    let type: MessageType
+    
+    init(_ text: String, type: MessageType, date: Date) {
+        self.text = text
+        self.type = type
+        self.date = date
+    }
+
+    init(_ text: String, type: MessageType) {
+        self.init(text, type: type, date: Date())
+    }
+}
